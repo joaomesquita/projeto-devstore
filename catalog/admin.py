ca33bef3b2ca57a28e99102913a96d03c35ea8e7
João Mesquita <contatomesquita@outlook.com>
2018-09-18 16:47:41 -0300

    :art: impl. dos admin ProductVariationInline e ProductImagemInline

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductVariation, ProductImage
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
@@ -7,10 +7,19 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name', 'slug']
     list_filter = ['created', 'modified']
 
+class ProductVariationInline(admin.TabularInline):
+    model = ProductVariation
+    extra = 1
+
+class ProductImagemInline(admin.TabularInline):
+    model = ProductImage
+    extra = 4
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'category', 'created', 'modified']
     search_fields = ['name', 'slug', 'category__name']
     list_filter = ['created', 'modified']
+    inlines = [ProductVariationInline, ProductImagemInline]
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
