7ff197a556147b1177caf5e338dbec3a951640e6
João Mesquita <contatomesquita@outlook.com>
2018-09-20 16:29:25 -0300

    :white_check_mark: impl. do "ManyToManyField", alteração do modelo "ProductVariation" para "Size" e impl. do modelo intermediário "ProductVariation"

--- catalog/models.py
+++ catalog/models.py
@@ -20,6 +20,17 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('catalog:category', kwargs={'slug': self.slug})
 
+class Size(models.Model):
+    size = models.CharField('Tamanho', max_length=3, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Tamanho'
+        verbose_name_plural = 'Tamanhos'
+        ordering = ['-size']
+
+    def __str__(self):
+        return self.size
+
 class Product(models.Model):
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Identificador', max_length=100)
@@ -28,6 +39,7 @@ class Product(models.Model):
     price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     sale = models.DecimalField('Preço Promocional', decimal_places=2, max_digits=8, null=True, blank=True)
     quantity = models.IntegerField('Quantidade', null=True, blank=True)
+    size = models.ManyToManyField('catalog.Size', through='catalog.ProductVariation')
     created = models.DateTimeField('Criado em', auto_now_add=True)
     modified = models.DateTimeField('Modificado em', auto_now=True)
 
@@ -43,15 +55,17 @@ class Product(models.Model):
         return reverse('catalog:product', kwargs={'slug': self.slug})
 
 class ProductVariation(models.Model):
-    product = models.ForeignKey('catalog.Product', verbose_name='Produto', on_delete=models.CASCADE)
-    size = models.CharField('Tamanho', max_length=3, null=True, blank=True)
+    sizes = models.ForeignKey('catalog.Size', verbose_name='Tamanho', on_delete=models.CASCADE)
+    products = models.ForeignKey('catalog.Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField('Quantidade', null=True)
 
     class Meta:
         verbose_name = 'Variação'
         verbose_name_plural = 'Variações'
+        ordering = ['sizes']
 
     def __str__(self):
-        return self.product.name
+        return self.products.name
 
 class ProductImage(models.Model):
     product = models.ForeignKey('catalog.Product', verbose_name='Produto', on_delete=models.CASCADE)
