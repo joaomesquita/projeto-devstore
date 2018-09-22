b992312605162008889d5af5e41317baa85d5451
João Mesquita <contatomesquita@outlook.com>
2018-09-21 21:35:21 -0300

    :arrow_down: comentado o import cloudinary e configuração

--- devstore/settings.py
+++ devstore/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import cloudinary, cloudinary.uploader, cloudinary.api
+#import cloudinary, cloudinary.uploader, cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,10 +131,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Configuração do cloudinary 
 
-cloudinary.config (
-    cloud_name = 'devstore',
-    api_key = '719213124598396',
-    api_secret = 'hFQpJLFrrwJekuflLRxWhONcioo'
-)
+#cloudinary.config (
+#    cloud_name = 'devstore',
+#    api_key = '719213124598396',
+#    api_secret = 'hFQpJLFrrwJekuflLRxWhONcioo'
+#)
