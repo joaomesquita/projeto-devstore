d777e0ba8b2a40fd61ced0372903e5a0c0ca8c35
João Mesquita <contatomesquita@outlook.com>
2018-11-19 21:14:40 -0200

    :white_check_mark: implementaçao de infomações da dashboard admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "Painel de Controle - Devstore"
+admin.site.site_title = "Devstore"
+admin.site.index_title = "Painel de Controle"
